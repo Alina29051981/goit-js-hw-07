4f5678546055d174aa2b667273b577192ecbd701
ostaliva <ostaliva@ukr.net>
2025-06-24 19:00:12 +0300

    change task-2 and style file and dilete image

--- js/task-2.js
+++ js/task-2.js
@@ -1,53 +1,35 @@
 const images = [
-  // {
-  //   url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-  //   alt: "White and Black Long Fur Cat",
-  // },
-  // {
-  //   url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-  //   alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  // },
-  // {
-  //   url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-  //   alt: "Group of Horses Running",
-  // },
-  // {
-  //   url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-  //   alt: "Alpine Spring Meadows",
-  // },
-  // {
-  //   url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-  //   alt: "Nature Landscape",
-  // },
-  // {
-  //   url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-  //   alt: "Lighthouse Coast Sea",
-  // },
-  { src: "./images/1-1-min.jpg" },
-  { src: "./images/1-2-min.jpg" },
-  { src: "./images/1-3-min.jpg" },
-  { src: "./images/2-1-min.jpg" },
-  { src: "./images/2-2-min.jpg" },
-  { src: "./images/2-3-min.jpg" },
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  },
 ];
 
 const gallery = document.querySelector(".gallery");
 
-// const markup = images
-//   .map((image) => {
-//     if (image.url && image.alt) {
-//       return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
-//     } else if (image.src) {
-//       return `<li><img src="${image.src}" alt="JPG"></li>`;
-//     }
-//   })
-// .join("");
-
 const markup = images
-  .map((image) => {
-    image.src;
-    return `<li><img src="${image.src}" alt="JPG"></li>`;
-  })
+  .filter(({ url, alt }) => url && alt)
+  .map(({ url, alt }) => `<li><img src="${url}" alt="${alt}"></li>`)
   .join("");
 
 gallery.insertAdjacentHTML("beforeend", markup);
